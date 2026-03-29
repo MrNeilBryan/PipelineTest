0c72ced4d2091b0297c30213192c0ab524543643
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 12:59:51 +0100

    Update 010_Create_Users_Table.sql

--- migrations/010_Create_Users_Table.sql
+++ migrations/010_Create_Users_Table.sql
@@ -1,14 +1,18 @@
 ----------------------------------------------------------
 -- Create the Users Table
 ----------------------------------------------------------
+DECLARE @Message NVARCHAR(4000) = N'';
 
 IF OBJECT_ID('dbo.Users') IS NULL
   BEGIN
     CREATE TABLE dbo.Users (ID INT PRIMARY KEY, [Name] NVARCHAR(100));
-    PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users created.';  
+    SET @Message  =  CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users created.';  
+    INSERT dbo.PipelineAudit ([Message]) SELECT @Message;
+  END
+  ELSE
+  BEGIN
+    SET @Message  =  CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
+    INSERT dbo.PipelineAudit ([Message]) SELECT @Message;
   END
  
-ELSE
-BEGIN
-  PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
-END
+ 
