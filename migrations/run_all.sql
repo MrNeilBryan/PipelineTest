f076b66b75cae441ff541c51667998fff3b04749
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 13:43:22 +0100

    Update migration script to use new user table file

--- migrations/run_all.sql
+++ migrations/run_all.sql
@@ -1,5 +1,5 @@
 /* This file tells Azure exactly which order to run your scripts */
 
 :r ./migrations/000_Create_Pipeline_Audit_Table.sql
-:r ./migrations/001_Create_Users_Table.sql
+:r ./migrations/010_Create_Users_Table.sql
 /* Add new files here as you create them */
