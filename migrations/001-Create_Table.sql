c8e65c0c0402a3b19d1bacb9a865700723cb86e3
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 12:34:59 +0100

    Create Users table if it does not exist

--- migrations/001-Create_Table.sql
+++ migrations/001-Create_Table.sql
@@ -1,4 +1,7 @@
 
+
+
+
 IF OBJECT_ID('dbo.Users') IS NULL
   BEGIN
     CREATE TABLE dbo.Users (ID INT PRIMARY KEY, [Name] NVARCHAR(100));
@@ -9,3 +12,6 @@ ELSE
 BEGIN
   PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
 END
+
+
+
