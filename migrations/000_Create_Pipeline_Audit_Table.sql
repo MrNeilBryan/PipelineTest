c79493e2a3d134f54fa26de5bc760ab40d0f611b
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 12:55:39 +0100

    Create PipelineAudit table for audit logging

---
+++ migrations/000_Create_Pipeline_Audit_Table.sql
@@ -0,0 +1,12 @@
+----------------------------------------------------------
+-- Create pipline audit table
+----------------------------------------------------------
+
+IF OBJECT_ID('dbo.PipelineAudit') IS NULL
+BEGIN
+  CREATE TABLE dbo.PipelineAudit (ID            INT             NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+                                  [Message]     NVARCHAR(4000)  NOT NULL,
+                                  [Timestamp]   DATETIME        NOT NULL DEFAULT GETDATE());
+END
+ 
+ 
