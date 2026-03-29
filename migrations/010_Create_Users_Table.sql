cf2971162d383661391be2cb98e1ffa9b2d1178e
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 12:50:37 +0100

    Update 010_Create_Users_Table.sql

--- migrations/010_Create_Users_Table.sql
+++ migrations/010_Create_Users_Table.sql
@@ -1,1 +1,14 @@
+----------------------------------------------------------
+-- Create the Users Table
+----------------------------------------------------------
 
+IF OBJECT_ID('dbo.Users') IS NULL
+  BEGIN
+    CREATE TABLE dbo.Users (ID INT PRIMARY KEY, [Name] NVARCHAR(100));
+    PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users created.';  
+  END
+ 
+ELSE
+BEGIN
+  PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
+END
