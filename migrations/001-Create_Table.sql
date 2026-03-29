606b872f256190e6793e7d1b98b991d0f51716df
MrNeilBryan <mr.neil.bryan@gmail.com>
2026-03-29 12:44:34 +0100

    Refactor table creation logic for Users table

--- migrations/001-Create_Table.sql
+++ migrations/001-Create_Table.sql
@@ -9,11 +9,8 @@ IF OBJECT_ID('dbo.Users') IS NULL
     CREATE TABLE dbo.Users (ID INT PRIMARY KEY, [Name] NVARCHAR(100));
     PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users created.';  
   END
-END
+ 
 ELSE
-BEGIN
-  PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
-END
-
-
-
+  BEGIN
+    PRINT CONVERT(NVARCHAR(4000), GETDATE(), 120) + ' - Table dbo.Users already exists.';  
+  END
